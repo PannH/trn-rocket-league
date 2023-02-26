7bd0e80b9e4f8a222cb2438a4e400cc24d35ef89
PannH <pannh2609@gmail.com>
2023-02-26 22:14:01 +0100

    fix: return the error when the profile was not found

--- dist/functions/fetchProfile.js
+++ dist/functions/fetchProfile.js
@@ -9,14 +9,14 @@ async function fetchProfile(username, platform) {
     return new Promise((resolve, reject) => {
         const AVAILABLE_PLATFORMS = ['epic', 'psn', 'steam', 'xbl'];
         if (!AVAILABLE_PLATFORMS.includes(platform))
-            reject(new TypeError(`The platform must be one of: ${AVAILABLE_PLATFORMS.join(', ')}`));
+            return reject(new TypeError(`The platform must be one of: ${AVAILABLE_PLATFORMS.join(', ')}`));
         const PROFILE_API_BASE_URL = 'https://api.tracker.gg/api/v2/rocket-league/standard/profile';
         (0, child_process_1.exec)(`curl --user-agent 'Chrome/79' --url ${PROFILE_API_BASE_URL}/${platform}/${username}`, (error, stdout, stderr) => {
             const response = JSON.parse(stdout);
             if (response?.errors && response?.errors[0]?.code === 'CollectorResultStatus::NotFound')
-                reject(new Error(`No profile found for '${platform}/${username}'`));
+                return reject(new Error(`No profile found for '${platform}/${username}'`));
             const { data } = response;
-            resolve(new classes_1.Profile(data));
+            return resolve(new classes_1.Profile(data));
         });
     });
 }
