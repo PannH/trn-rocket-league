acabab211f71d564b51f4864526ca9592a6b99cb
PannH <pannh2609@gmail.com>
2023-03-01 00:55:49 +0100

    fix: resolve cannot read stats of undefined

--- dist/classes/ProfileStats.js
+++ dist/classes/ProfileStats.js
@@ -22,31 +22,33 @@ class ProfileStats {
         const doubleStats = data.segments.find((segment) => segment.metadata.name === 'Ranked Doubles 2v2');
         const standardStats = data.segments.find((segment) => segment.metadata.name === 'Ranked Standard 3v3');
         this.ranked = {
-            duel: new _1.PlaylistStats(duelStats),
-            double: new _1.PlaylistStats(doubleStats),
-            standard: new _1.PlaylistStats(standardStats)
+            duel: duelStats ? new _1.PlaylistStats(duelStats) : undefined,
+            double: doubleStats ? new _1.PlaylistStats(doubleStats) : undefined,
+            standard: duelStats ? new _1.PlaylistStats(standardStats) : undefined
         };
         const dropshotStats = data.segments.find((segment) => segment.metadata.name === 'Dropshot');
         const hoopsStats = data.segments.find((segment) => segment.metadata.name === 'Hoops');
         const rumbleStats = data.segments.find((segment) => segment.metadata.name === 'Rumble');
         const snowdayStats = data.segments.find((segment) => segment.metadata.name === 'Snowday');
         this.extra = {
-            dropshot: new _1.PlaylistStats(dropshotStats),
-            hoops: new _1.PlaylistStats(hoopsStats),
-            rumble: new _1.PlaylistStats(rumbleStats),
-            snowday: new _1.PlaylistStats(snowdayStats)
+            dropshot: dropshotStats ? new _1.PlaylistStats(dropshotStats) : undefined,
+            hoops: hoopsStats ? new _1.PlaylistStats(hoopsStats) : undefined,
+            rumble: rumbleStats ? new _1.PlaylistStats(rumbleStats) : undefined,
+            snowday: snowdayStats ? new _1.PlaylistStats(snowdayStats) : undefined
         };
         const unrankedStats = data.segments.find((segment) => segment.metadata.name === 'Un-Ranked');
-        this.unranked = new _1.PlaylistStats(unrankedStats);
-        this.totalMatchesPlayed =
-            this.ranked.duel.matchesPlayed +
-                this.ranked.double.matchesPlayed +
-                this.ranked.standard.matchesPlayed +
-                this.extra.dropshot.matchesPlayed +
-                this.extra.hoops.matchesPlayed +
-                this.extra.rumble.matchesPlayed +
-                this.extra.snowday.matchesPlayed +
-                this.unranked.matchesPlayed;
+        this.unranked = unrankedStats ? new _1.PlaylistStats(unrankedStats) : undefined;
+        const segmentsMatchesPlayed = [
+            this.ranked.duel?.matchesPlayed,
+            this.ranked.double?.matchesPlayed,
+            this.ranked.standard?.matchesPlayed,
+            this.extra.dropshot?.matchesPlayed,
+            this.extra.hoops?.matchesPlayed,
+            this.extra.rumble?.matchesPlayed,
+            this.extra.snowday?.matchesPlayed,
+            this.unranked?.matchesPlayed
+        ].filter((element) => !!element);
+        this.totalMatchesPlayed = segmentsMatchesPlayed.reduce((total, matchesPlayed) => total + matchesPlayed, 0);
     }
 }
 exports.default = ProfileStats;
