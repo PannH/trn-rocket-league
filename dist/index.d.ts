b75f93a39b354c3cd7053d9f7d8cfea00b7526c7
PannH <pannh2609@gmail.com>
2023-02-28 21:56:30 +0100

    feat: export every types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import { fetchProfile } from './functions';
-import type { Platform } from './types';
-import type { Profile } from './classes';
-export { fetchProfile, Platform, Profile };
+import type { Platform, ExtraStats, OverviewStats, RankedStats, Rank, RankDivision, RankTier, LinkedAccount } from './types';
+import type { Profile, PlaylistStats, ProfileStats } from './classes';
+export { fetchProfile, Platform, Profile, ExtraStats, OverviewStats, RankedStats, Rank, RankDivision, RankTier, LinkedAccount, PlaylistStats, ProfileStats };
